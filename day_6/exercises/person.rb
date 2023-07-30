f6226b9df39e758a221df8223dafdeb8d620cfc2
Scott DeVoss <scottdevoss@yahoo.com>
2023-07-30 15:46:19 -0600

    add day 6

--- day_6/exercises/person.rb
+++ day_6/exercises/person.rb
@@ -2,4 +2,39 @@
 # Call all person methods below the class and print results
 # to the terminal that show the methods in action.
 
-# YOUR CODE HERE
+class Person
+
+    attr_reader :name, :age, :hungry, :thirsty
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+        @hungry = true
+        @thirsty = false
+    end 
+
+    def eat 
+        @hungry = false
+    end
+
+    def dont_drink
+        @thirsty = true
+    end 
+
+end 
+
+person1 = Person.new("Scott", 27)
+
+
+p person1
+p person1.name
+p person1.age
+p person1.hungry
+p person1.thirsty
+
+person1.eat
+person1.dont_drink
+
+p person1.hungry
+p person1.thirsty
+
